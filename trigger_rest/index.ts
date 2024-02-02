af6f2df32abdfee8a1ad1f00759faa5272fd29bb
josemr22 <josemr.99@outlook.com>
2024-02-01 23:35:00 -0500

    clean package-lock

--- trigger_rest/index.ts
+++
@@ -1,9 +0,0 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { AzureHttpAdapter } from "@nestjs/azure-func-http";
-import { createApp } from "src/main.azure";
-
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    AzureHttpAdapter.handle(createApp, context, req);
-};
-
-export default httpTrigger;
